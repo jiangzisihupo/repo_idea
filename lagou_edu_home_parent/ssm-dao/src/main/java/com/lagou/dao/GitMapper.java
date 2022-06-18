7d3fd29fe505a1991a4fc7d916beac914e5a6966
guoqiangpan <1007101891@qq.com>
2022-06-19 00:59:15 +0800

    add GitMapper

---
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/GitMapper.java
@@ -0,0 +1,8 @@
+package com.lagou.dao;
+
+/**************************************
+ * @author pan
+ * @version 2022/6/19 0:58
+ **************************************/
+public interface GitMapper {
+}
